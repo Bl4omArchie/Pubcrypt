9dab5d7b74e555bd0bc569310f47c6b503e17e64
archie <saitama33000@protonmail.com>
2023-04-10 00:28:09 +0200

    new scheme: oaep and pkcs

--- pubcrypt/number/random.py
+++ pubcrypt/number/random.py
@@ -1,4 +1,5 @@
 from random import randrange
+from math import ceil
 
 
 def RBG(nBits):
@@ -7,8 +8,17 @@ def RBG(nBits):
 def RNG(nBits):
     return randrange(pow(2, nBits-1)+1, pow(2, nBits)-1)
 
+
 def int_to_bytes(x: int) :
     return x.to_bytes((x.bit_length() + 7) // 8, 'big')
     
 def bytes_to_int(xbytes: bytes) :
     return int.from_bytes(xbytes, 'big')
+
+
+def MGF(seed, mask_len, hash_len):
+    if mask_len > pow(2, 32): raise ValueError("Mask too long")
+
+    T = ""
+    for i in range(ceil(mask_len, hash_len)-1):
+        
