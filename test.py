fb4143aa8d042d5e6cdc2b2f3c9e725be7f7fbf5
Bl4omArchie <57256763+Bl4omArchie@users.noreply.github.com>
2023-08-06 01:29:46 +0200

    test for a parallel prime_generator, thiking for a code benchmark with cProfile

--- test.py
+++ test.py
@@ -8,9 +8,10 @@ from benchmark.graph import *
 
 
 def simulation():
-    time = get_time(50, 2048)
+    time = get_time(10, 2048)
     make_simple_plot(time, "generate() with new binary algorithm", "generate()", "red")
 
+    """
     time1, time2 = get_time_converting(1000)
     make_n_plot("plot", (time1, time2), "Comparing conversion integer to binary", ("original", "new"), ("red", "green"))
 
@@ -22,7 +23,7 @@ def simulation():
 
     time1, time2, time3 = get_time_gcd(100)
     make_n_plot("point", (time1, time2, time3), "Comparing several gcd function", ("old", "new", "python"), ("red", "green", "purple"))
-    
+    """
 
 if __name__ == "__main__":
     simulation()
